1bf12aeab202505b1e5f17f58717f31c54b7a6b3
Esther <eagyare91@gmail.com>
2021-02-26 23:53:16 +0000

    first commit

---
+++ authentication.js
@@ -0,0 +1,16 @@
+
+
+//var spotifyArtist = document.getElementById('spotify-data');
+
+        //$.ajax({
+          //url: "https://api.spotify.com/v1/me/top/artists",
+          ////headers: {
+              //    'Authorization': 'Bearer ' + access_token
+               // },
+              
+                //success: function(response) {
+                  //spotifyArtist.innerHTML += `
+                  //<h2>${artists}</h2>`
+              //  },
+            //  })
+            //})
